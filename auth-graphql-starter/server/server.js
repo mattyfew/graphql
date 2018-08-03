892aaff4b7d3a04e73e4f3bcbf91f897b3bcfccb
Matt Fewer <mattfewer@hotmail.com>
2018-08-03 21:35:43 +0200

    beginning of proj done

--- auth-graphql-starter/server/server.js
+++ auth-graphql-starter/server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb://matthew:password>@ds141454.mlab.com:41454/auth';
+const MONGO_URI = 'mongodb://matthew:password1@ds141454.mlab.com:41454/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
